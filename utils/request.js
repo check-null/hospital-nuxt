a81e1aa3e3d50a4431455af72968d5d84929e498
checkNull <qw1124sdo8@live.com>
2021-11-30 18:08:13 +0800

    cookie

--- utils/request.js
+++ utils/request.js
@@ -1,18 +1,23 @@
 import axios from 'axios'
 import { MessageBox, Message } from 'element-ui'
+import cookie from 'js-cookie'
 // 创建axios实例 
 const service = axios.create({
     // 请求超时时间 
-    baseURL: 'http://localhost',
+    baseURL: 'http://hosp.com',
     timeout: 15000
 })
 // http request 拦截器 
-service.interceptors.request.use(config => {
-    // token 先不处理，后续使用时在完善
-
-    return config
-},
-    err => { return Promise.reject(err) })
+service.interceptors.request.use(
+    config => {
+        // token 先不处理，后续使用时在完善
+        if (cookie.get('token')) {
+            config.headers['token'] = cookie.get('token')
+        }
+        return config
+    },
+    err => { return Promise.reject(err) }
+)
 // http response 拦截器 
 service.interceptors.response.use(
     response => {
@@ -20,7 +25,8 @@ service.interceptors.response.use(
         if (response.data.code !== 200) {
             Message({
                 message: response.data.message,
-                type: 'error', duration: 5 * 1000
+                type: 'error', 
+                duration: 5 * 1000
             })
             return Promise.reject(response.data)
         } else {
