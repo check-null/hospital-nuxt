34803f28d3e4b003b2b8061336a11290e3ef519a
checkNull <qw1124sdo8@live.com>
2022-03-14 18:01:20 +0800

    支付宝回调2

--- pages/order/alipay/notify.vue
+++ pages/order/alipay/notify.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>1111</div>
+  <div>支付成功</div>
 </template>
 
 <script>
@@ -19,11 +19,18 @@ export default {
   // 监控data中的数据变化
   watch: {},
   // 方法集合
-  methods: {},
+  methods: {
+    reload() {
+      let param = location.search.substring(1).split('&');
+      console.log(param);
+    }
+  },
   beforeCreate() {}, // 生命周期-创建之前
   created() {}, // 生命周期-创建完成（可以访问当前this实例）
   beforeMount() {}, // 生命周期-挂载之前
-  mounted() {}, // 生命周期-挂载完成（可以访问DOM元素）
+  mounted() {
+    this.reload();
+  }, // 生命周期-挂载完成（可以访问DOM元素）
   activated() {}, // 如果页面有keep-alive缓存功能，这个函数会触发
   beforeUpdate() {}, // 生命周期-更新之前
   updated() {}, // 生命周期-更新之后
